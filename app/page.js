05c11f55b2f4012b06123fd11d272b16351338c8
Muhtasim Mushfiq <muhtasim01@gmail.com>
2024-09-03 21:34:36 -0400

    Finished landing page

--- app/page.js
+++ app/page.js
@@ -31,20 +31,20 @@ export default function Home() {
         textAlign: 'center',
         my: 4,
       }}>
-        <Typography variant = 'h2'> Welcome to Flashcard SaaS </Typography>
-        <Typography variant = 'h5'> 
+        <Typography variant = 'h2' gutterBottom> Welcome to Flashcard SaaS </Typography>
+        <Typography variant = 'h5' gutterBottom> 
           {' '}
           The easiest way to make flashcards from scratch
         </Typography>
         <Button variant = "contained" color ='primary' sx = {{mt: 2}}> Get Started</Button>
       </Box>
       <Box sx = {{my:6}}>
-        <Typography variant = "h4" components = "h2">
+        <Typography variant = "h4" gutterBottom>
           Features
         </Typography>
         <Grid container spacing = {4}>
           <Grid item xs = {12} md={4}>
-          <Typography variant="h6">
+          <Typography variant="h6" gutterBottom>
             Easy Text Input
           </Typography>
           <Typography> 
@@ -54,7 +54,7 @@ export default function Home() {
           </Grid>
 
           <Grid item xs = {12} md={4}>
-          <Typography variant="h6">
+          <Typography variant="h6" gutterBottom>
             Smart Flashcards
           </Typography>
           <Typography> 
@@ -64,7 +64,7 @@ export default function Home() {
           </Grid>
 
           <Grid item xs = {12} md={4}>
-          <Typography variant="h6">
+          <Typography variant="h6" gutterBottom>
             Accessible Anywhere
           </Typography>
           <Typography> 
@@ -78,44 +78,48 @@ export default function Home() {
         textAlign: 'center',
         my: 6,
       }}>
-        <Typography variant = 'h4'>Pricing</Typography>
+        <Typography variant = 'h4' gutterBottom>Pricing</Typography>
         <Grid container spacing = {4}>
-          <Grid item xs = {12} md={4}>
+          <Grid item xs = {12} md={6}>
             <Box sx= {{
               p: 3,
               border: '1px solid',
               borderColor: 'grey.300',
               borderRadius: 2,
             }}>
-              <Typography variant="h5">Basic</Typography>
-              <Typography variant="h6">$5 / month</Typography>
+              <Typography variant="h5" gutterBottom>Basic</Typography>
+              <Typography variant="h6" gutterBottom>$5 / month</Typography>
               <Typography> 
                 {' '}
                 Access to basic flashcard features and limited storage
               </Typography>
-              <Button variant = "container" color = 'primary'> 
+              <Button variant = "contained" color = 'primary' sx={{mt:2}}> 
                 Choose basic
               </Button>
             </Box>
           </Grid>
-          <Grid item xs = {12} md={4}>
-            <Typography variant="h6">
-              Smart Flashcards
-            </Typography>
-            <Typography> 
-              {' '}
-              Our AI inteligently breaks down your text into concise flashcards, perfect for studying
-            </Typography>
-          </Grid>
-          <Grid item xs = {12} md={4}>
-            <Typography variant="h6">
-              Accessible Anywhere
-            </Typography>
-            <Typography> 
-              {' '}
-              Access your flashcards from any device, at any time. Study on the go with ease.
-            </Typography>
-          </Grid>
+          <Grid item xs = {12} md={6}>
+          <Box sx= {{
+              p: 3,
+              border: '1px solid',
+              borderColor: 'grey.300',
+              borderRadius: 2,
+            }}>
+              <Typography variant="h5" gutterBottom>
+                Pro
+              </Typography>
+              <Typography variant="h6" gutterBottom>
+                $10 / month
+                </Typography>
+              <Typography> 
+                {' '}
+                Unlimited flashcards and storage, with priority support.
+              </Typography>
+              <Button variant = "contained" color = 'primary' sx={{mt:2}}> 
+                Choose pro
+              </Button>
+            </Box>
+            </Grid>
         </Grid>
       </Box>
     </Container>
