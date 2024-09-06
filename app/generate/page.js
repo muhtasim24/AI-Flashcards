67de7990262a3825423adc17a9575934e290d649
Muhtasim Mushfiq <muhtasim01@gmail.com>
2024-09-05 22:38:39 -0400

    created some helper functions and exported database

--- app/generate/page.js
+++ app/generate/page.js
@@ -14,4 +14,33 @@ export default function Generate(){
     const [name, setName] = useState('')
     const [open, setOpen] = useState(false) // for modals
     const router = useRouter // used to route between pages
+
+    // helper fucntion will submit our text to generate
+    // sending to our api
+    const handleSubmit = async () => {
+        fetch('api/generate', {
+            method:'POST', // sending the method, we are POST
+            body: text, // sending in the body, the text
+        })
+            .then((res) => res.json()) // .then with the response, and process as JSON
+            .then((data > setFlashcards(data))) // take the data, then setFlashcards with the data, response is going to be an array of flashcards
+    }
+
+    // helper function to hanlde flipping the flashcard
+    const handleCardClick = (id) => { // get the id of the card
+        setFlipped((prev) => ({
+            ...prev,    // keep all the cards the same
+            [id]: !prev[id] // card with the specific id, flip it, if its True, becomes false, vice versa
+        }))
+    }
+    // helper function to open modals
+    const handleOpen = () => {
+        setOpenI(true)
+    }
+
+    // helper function to close modals
+    const handleClose = () => {
+        setOpenI(false)
+    }
+
 }   
