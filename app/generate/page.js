7ec4c0af99bb05cad76eae819d8abd1af4a8483a
Muhtasim Mushfiq <muhtasim01@gmail.com>
2024-09-06 17:27:52 -0400

    worked on flipping animation of flashcard

--- app/generate/page.js
+++ app/generate/page.js
@@ -3,6 +3,7 @@
 'use client'
 
 import { useUser } from "@clerk/nextjs"
+import { Box, Container, TextField, Typography, Paper, Button, Grid, Card, CardActionArea, CardContent } from "@mui/material"
 import { getDoc, writeBatch } from "firebase/firestore"
 import { useRouter } from "next/navigation"
 import { useState } from "react"
@@ -83,4 +84,100 @@ export default function Generate(){
         handleClose()
         router.push('/flashcards') // pushes to flashcard page
     }
+
+    return (
+    <Container maxWidth="md">
+        <Box sx = {{
+            mt: 4, mb: 6, display: 'flex', flexDirection: 'column', alignItems: 'center',
+        }}
+        >
+            <Typography variant = "h4"> Generate flashcards </Typography>
+            <Paper sx = {{p: 4, width: "100%"}}>
+                <TextField value = {text}
+                onChange={(e) => setText(e.target.value)} label = "Enter text"
+                fullWidth
+                multiline
+                rows={4}
+                variant = "outlined"
+                sx = {{
+                    mb: 2,
+                }}
+                />
+                <Button
+                    variant='contained'
+                    color='primary'
+                    onClick={handleSubmit}
+                    fullWidth
+                    >
+                        Submit
+                    </Button>
+            </Paper>
+        </Box>
+
+        {flashcards.length > 0 && (
+            <Box sx={{mt: 4}}>
+                <Typography variant = "h5"> Flashcards Preview</Typography>
+                <Grid container spacing={3}> 
+                    {flashcards.map((flashcard, index) => (
+                        <Grid item xs = {12} sm = {6} md = {4} key = {index}>
+                            <Card>
+                                <CardActionArea
+                                    onClick={() => {
+                                        handleCardClick(index) 
+                                    }}
+                                    >
+                                    <CardContent>
+                                        <Box sx={{
+                                            perspective: '1000px',
+                                            '& > div': {
+                                                transition: 'transform 0.6s',
+                                                transformStyle: 'preserve-3d',
+                                                position: 'relative',
+                                                width: '100%',
+                                                height: '200px',
+                                                boxShadow: '0 4px 8px rgba(0,0,0, 0.2)',
+                                                transfrom: flipped[index]
+                                                    ? 'rotateY(180deg)'
+                                                    : 'rotateY(0deg)',
+                                            },
+                                            '& > div > div': {
+                                                position: 'absolute',
+                                                width: '100%%',
+                                                height: '100%',
+                                                backfaceVisibility: 'hidden',
+                                                display: 'flex',
+                                                justifyContent: 'center',
+                                                alignItems: 'center',
+                                                padding: 2,
+                                                boxSizing: 'border-box'
+                                            },
+                                            '& > div > div:nth-of-type(2)': {
+                                                transform: 'rotateY(180deg)',
+                                            },
+                                        }}
+                                        >
+                                            <div>
+                                                <div>
+                                                    <Typography varaint = "h5" component = 'div' >
+                                                        {flashcard.front}    
+                                                    </Typography>  
+                                                </div>
+                                                <div>
+                                                    <Typography varaint = "h5" component = 'div' >
+                                                        {flashcard.back}    
+                                                    </Typography>  
+                                                </div>
+                                            </div>
+
+                                        </Box>
+                                    </CardContent>
+                                </CardActionArea>
+                            </Card>
+                        </Grid>
+                    ))}
+                </Grid>
+            </Box>
+        )}
+    </Container>
+    )
 }   
